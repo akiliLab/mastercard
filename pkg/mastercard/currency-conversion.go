6ca9436b634b878388011a1db5959fea0001a863
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-04-22 11:52:32 +0300

    added currency conversion response

--- pkg/mastercard/currency-conversion.go
+++ pkg/mastercard/currency-conversion.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/url"
+	"strconv"
 
 	mastercardpb "github.com/ubunifupay/mastercard/pb"
 )
@@ -15,7 +16,7 @@ import (
 // Default searchMethod equals to ExactMatch which returns either 1 or 0 merchant while
 // FuzzyMatch returns from 0 to 20 merchants with a Matching confidence field that scores
 // from 0 to 100
-func (c *Client) GetCurrencyConversion(request *mastercardpb.MastercardCurrencyConversionRequest) (*mastercardpb.MerchantIDs, error) {
+func (c *Client) GetCurrencyConversion(request *mastercardpb.MastercardCurrencyConversionRequest) (*mastercardpb.CurrencyResponse, error) {
 	urlFull := &url.URL{
 		Scheme: "https",
 		Host:   c.BaseURL,
@@ -26,25 +27,28 @@ func (c *Client) GetCurrencyConversion(request *mastercardpb.MastercardCurrencyC
 	params.Set("Format", "XML")
 	params.Set("fxDate", request.FxDate)
 	params.Set("transCurr", request.TransCurr)
+	params.Set("crdhldBillCurr", request.CrdhldBillCurr)
+	params.Set("bankFee", strconv.FormatInt(request.BankFee, 10))
+	params.Set("transAmt", fmt.Sprintf("%f", request.TransAmt))
 
 	resp, err := c.oauthClient.Get(c.httpClient, &c.oauthClient.Credentials, urlFull.String(), params)
 	if err != nil {
-		log.Println("Error in mastercard merchantId: ", err.Error())
+		log.Println("Error in mastercard currencyconversion: ", err.Error())
 		return nil, err
 	}
 	if resp.StatusCode != 200 {
-		log.Println("Error in mastercard merchantId: status: ", resp.Status, "code: ", resp.StatusCode)
+		log.Println("Error in mastercard currencyconversion: status: ", resp.Status, "code: ", resp.StatusCode)
 		return nil, errors.New("Failed request to mastercard api : " + resp.Status)
 	}
 	defer resp.Body.Close()
 
-	merchantIds := &mastercardpb.MerchantIDs{}
+	dataConversionResponse := &mastercardpb.CurrencyResponse{}
 	serializedBody, _ := ioutil.ReadAll(resp.Body)
-	err = xml.Unmarshal(serializedBody, merchantIds)
+	err = xml.Unmarshal(serializedBody, dataConversionResponse)
 	if err != nil {
-		return nil, errors.New("Error in mastercard merchant id: Couldn't parse XML response")
+		return nil, errors.New("Error in mastercard currencyconversion: Couldn't parse XML response")
 	}
 	bodyString := string(serializedBody)
 	fmt.Println("Body: ", bodyString)
-	return merchantIds, nil
+	return dataConversionResponse, nil
 }
